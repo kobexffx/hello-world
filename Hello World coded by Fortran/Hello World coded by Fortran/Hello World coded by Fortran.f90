91b966bf576e9dedab86244e4d9fb4bf2150aec0
Ye Fe <xffx2009mvp@outlook.com>
2017-02-27 21:36:33 +0800

    vs commit test

---
+++ Hello World coded by Fortran/Hello World coded by Fortran/Hello World coded by Fortran.f90
@@ -0,0 +1,25 @@
+!  HelloWorldcodedbyFortran.f90 
+!
+!  FUNCTIONS:
+!  HelloWorldcodedbyFortran - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: HelloWorldcodedbyFortran
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program HelloWorldcodedbyFortran
+
+    implicit none
+
+    ! Variables
+
+    ! Body of HelloWorldcodedbyFortran
+    print *, 'Hello World'
+
+    end program HelloWorldcodedbyFortran
+
